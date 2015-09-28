8e1a07ca5b66ffcdd5e49c6cbd6a1d8eddeddfee
Hackbright Student <info@hackbrightacademy.com>
2015-09-28 16:22:28 -0700

    Cleaner working file

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,6 +1,8 @@
 # your code goes here
 
-def get_rating(filename):
+def make_restaurant_dict(filename):
+    """Opens a file of ratings and converts pairs into a dictionary.
+    """
     ratings_file = open(filename)
 
     restaurant_dict = {}
@@ -12,13 +14,23 @@ def get_rating(filename):
         restaurant_dict[restaurant_pair[0]] = restaurant_pair[1]
     # print restaurant_dict
 
+    return restaurant_dict
+
+
+def print_sorted_restaurants(restaurant_dict):
+    """Prints the restaurant pairs in alphabetical order.
+
+    Creates sorted list of restaurant names from dictionary keys, and then
+    loops through the dictionary to print the ratings (values).
+    """
     sorted_restaurants = sorted(restaurant_dict)
     # print sorted_restaurants
 
     for item in sorted_restaurants:
         print "{} is rated at {}.".format(item, restaurant_dict[item])
+
     return
 
-
-get_rating("scores.txt")
+restaurant_dict = make_restaurant_dict("scores.txt")
+print_sorted_restaurants(restaurant_dict)
 # print restaurant_dict
